1c62c42d1475b7c822efad2f689279bc577cbca4
Yevginiia <145446803+ukurere@users.noreply.github.com>
2024-05-22 15:34:05 +0300

    Add files via upload

---
+++ ChemistryLaboratory/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Please enter the maximum number of substances on the shelf:");
+
+
+string N = Console.ReadLine();
+int MAX = int.Parse(N);
+
+
+int cnt_of_substance = 0;
+
+while (true)
+{
+
+    string com = Console.ReadLine();
+    if (com == "add")
+    {
+        if (cnt_of_substance < MAX)
+            cnt_of_substance++;
+        else
+            Console.WriteLine("Number of substances on the shelf is max.");
+    }
+    else if (com == "div")
+    {
+        if (cnt_of_substance > 0)
+            cnt_of_substance--;
+        else
+            Console.WriteLine("Error");
+
+    }
+    else
+        Console.WriteLine("Your command is incorrect.");
+    Console.WriteLine(cnt_of_substance);
+}
