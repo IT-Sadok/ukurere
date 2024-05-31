ad8109d31146733514b7abc5cf3e6fea55265fe7
Yevginiia <145446803+ukurere@users.noreply.github.com>
2024-05-31 13:04:16 +0300

    Add files via upload

---
+++ ChemistryLaboratory/ChemistryLaboratory/Program.cs
@@ -0,0 +1,20 @@
+﻿using MyApp.Class;
+
+Console.WriteLine("Please enter the maximum number of substances on the shelf:");
+string input = Console.ReadLine();
+int maxSubstances;
+if (!int.TryParse(input, out maxSubstances))
+{
+    Console.WriteLine("Invalid number. Please restart the program and enter a valid number.");
+    return;
+}
+
+ChemistyLaboratory lab = new ChemistyLaboratory(maxSubstances);
+
+while (true)
+{
+    Console.WriteLine("Enter command (add/div):");
+    string command = Console.ReadLine();
+    lab.ExecuteCommand(command);
+    Console.WriteLine("Current number of substances: " + lab.GetCurrentSubstancesCount());
+}
---
+++ ChemistryLaboratory/Lesson1/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("Please enter the max number of substances on the shelf:");
+
+
+string N = Console.ReadLine();
+int MAX = int.Parse(N);
+
+
+int cnt_of_substance = 0;
+
+while(true)
+{
+
+    string com = Console.ReadLine();    
+    if (com == "add")
+    {
+        if(cnt_of_substance < MAX) 
+            cnt_of_substance++;
+        else
+            Console.WriteLine("Number of substances on the shelf is max.");
+    }
+    else if (com == "div")
+    {
+        if (cnt_of_substance > 0)
+            cnt_of_substance--;
+        else
+            Console.WriteLine("Error");
+
+    }
+    else
+        Console.WriteLine("Your command is incorrect.");
+    Console.WriteLine(cnt_of_substance);
+}
