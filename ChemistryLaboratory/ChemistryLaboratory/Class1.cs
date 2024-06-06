72f1ece64c93a75736a30bc934bd558bda3879d4
Yevginiia <145446803+ukurere@users.noreply.github.com>
2024-06-06 16:32:25 +0300

    Update Class1.cs

--- ChemistryLaboratory/ChemistryLaboratory/Class1.cs
+++ ChemistryLaboratory/ChemistryLaboratory/Class1.cs
@@ -1,11 +1,11 @@
-﻿using System;
+using System;
 
 namespace MyApp.Class
 {
     public class ChemistyLaboratory
     {
         private int maxSubstances;
-        private int currentSubstancesCount;
+        public int currentSubstancesCount;
 
         public ChemistyLaboratory(int max)
         {
@@ -13,54 +13,20 @@ namespace MyApp.Class
             currentSubstancesCount = 0;
         }
 
-        public void ExecuteCommand(string command)
-        {
-            if (command == "add")
-            {
-                Add();
-            }
-            else if (command == "div")
-            {
-                Div();
-            }
-            else
-            {
-                Error();
-            }
-        }
-
-        private void Add()
+        public void Add()
         {
             if (currentSubstancesCount < maxSubstances)
             {
                 currentSubstancesCount++;
             }
-            else
-            {
-                Console.WriteLine("Number of substances on the shelf is max.");
-            }
         }
 
-        private void Div()
+        public void Div()
         {
             if (currentSubstancesCount > 0)
             {
                 currentSubstancesCount--;
             }
-            else
-            {
-                Console.WriteLine("Error");
-            }
-        }
-
-        private void Error()
-        {
-            Console.WriteLine("Your command is incorrect.");
-        }
-
-        public int GetCurrentSubstancesCount()
-        {
-            return currentSubstancesCount;
         }
     }
 }
