4e13335546b3ad63e96a131ae5b6f48e9567e45c
Yevginiia <145446803+ukurere@users.noreply.github.com>
2024-06-22 00:30:49 +0300

    Update Program.cs

--- ChemistryLaboratory/ChemistryLaboratory/Program.cs
+++ ChemistryLaboratory/ChemistryLaboratory/Program.cs
@@ -10,49 +10,52 @@ namespace MyApp
             Console.WriteLine("Please enter the maximum number of substances on the shelf:");
             string input = Console.ReadLine();
             int maxSubstances;
+            string command;
+            int currentSubstancesCount = 0;
 
             while (!int.TryParse(input, out maxSubstances))
             {
-                Console.WriteLine("Invalid number. Please restart the program and enter a valid number.");
+                Console.WriteLine("Invalid number. Please enter a valid number.");
                 input = Console.ReadLine();
             }
 
-            ChemistyLaboratory lab = new ChemistyLaboratory(maxSubstances);
+            ChemistyLaboratory lab = new ChemistyLaboratory();
 
             while (true)
             {
                 Console.WriteLine("Enter command (add/div):");
 
-                lab.command = Console.ReadLine();
+                command = Console.ReadLine();
 
-                if (lab.command == "add")
+                try
                 {
-                    try
+                    if (command == "add")
                     {
-                        lab.Add();
+                        lab.Add(maxSubstances, ref currentSubstancesCount);
                     }
-                    catch (MaxLimitExceededException e)
+                    else if (command == "div")
                     {
-                        Console.WriteLine(e.Message);
+                        lab.Div(ref currentSubstancesCount);
+                    }
+                    else
+                    {
+                        throw new Exception("Your command is incorrect. Try again.");
                     }
                 }
-                else if (lab.command == "div")
+                catch (MaxLimitExceededException e)
                 {
-                    try
-                    {
-                        lab.Div();
-                    }
-                    catch (NoSubstancesException e)
-                    {
-                        Console.WriteLine(e.Message);
-                    }
+                    Console.WriteLine(e.Message);
                 }
-                else
+                catch (NoSubstancesException e)
                 {
-                    Console.WriteLine("Your command is incorrect.");
+                    Console.WriteLine(e.Message);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
                 }
 
-                Console.WriteLine("Current number of substances: " + lab.currentSubstancesCount);
+                Console.WriteLine("Current number of substances: " + currentSubstancesCount);
             }
         }
     }
