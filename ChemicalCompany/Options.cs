5b5fcc70203fe10edd3e3061f835e976dde64148
Yevginiia <145446803+ukurere@users.noreply.github.com>
2024-06-27 00:12:13 +0300

    Update Options.cs

--- ChemicalCompany/Options.cs
+++ ChemicalCompany/Options.cs
@@ -1,15 +1,39 @@
-﻿namespace ChemicalCompany
+using System.Data;
+using System.Text.Json;
+
+namespace ChemicalCompany
 {
-    internal class Options
+    public static class Options
     {
-        public int CurrentOfSubstance;
-        public void Add()
+        public static void Add(List<Substance> listOfSubstance, string filePath, Substance newSubstance)
         {
+                listOfSubstance.Add(newSubstance);
 
+                string jsonString = JsonSerializer.Serialize(listOfSubstance, new JsonSerializerOptions { WriteIndented = true });
+                File.WriteAllText(filePath, jsonString);
+            
         }
-        public void Remove()
-        {
 
+        public static void Remove(List<Substance> listOfSubstance, string filePath, int ip)
+        {
+            Substance substanceToRemove = listOfSubstance.Find(s => s.Ip == ip);
+
+            if (substanceToRemove != null)
+            {
+                listOfSubstance.Remove(substanceToRemove);
+
+                string jsonString = JsonSerializer.Serialize(listOfSubstance, new JsonSerializerOptions { WriteIndented = true });
+                File.WriteAllText(filePath, jsonString);
+            }
+            else
+            {
+                throw new Exception(); 
+            }            
+        }
+
+        public static int Current(List<Substance> listOfSubstance)
+        {
+            return listOfSubstance.Count;
         }
     }
 }
