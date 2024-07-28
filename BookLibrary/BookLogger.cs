1ff8dc8daecf6587b92a9d7f9de37f573b780cb4
Yevginiia <145446803+ukurere@users.noreply.github.com>
2024-07-28 12:32:31 +0300

    Update BookLogger.cs

--- BookLibrary/BookLogger.cs
+++ BookLibrary/BookLogger.cs
@@ -2,16 +2,14 @@ namespace BookLibrary
 {
     public static class BookLogger
     {
-        public static List<string> DisplayBooks(List<Book> books)
+        public static void DisplayBooks(List<Book> books)
         {
             List<string> bookList = new List<string>();
 
             foreach (var book in books)
             {
-                bookList.Add($"{book.Identifier}, {book.Title}, {book.Author}, {book.PublicationYear}");
+                Console.WriteLine($"{book.Identifier}, {book.Title}, {book.Author}, {book.PublicationYear}");
             }
-
-            return bookList;
         }
     }
 }
