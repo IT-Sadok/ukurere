114e015a579f4de4b494e6a46ea47094eb76feff
Yevginiia <145446803+ukurere@users.noreply.github.com>
2024-07-30 01:16:27 +0300

    Add files via upload

---
+++ Factory/Factory.cs
@@ -0,0 +1,63 @@
+﻿namespace Factory
+{
+    public class Factory
+    {
+        private readonly SemaphoreSlim _deliverSemaphore = new SemaphoreSlim(100, 100);
+        private readonly SemaphoreSlim _pickupSemaphore = new SemaphoreSlim(100, 100);
+        private int _parts = 0, _products = 0;
+
+        public async Task DeliverAsync(int Id)
+        {
+            await _deliverSemaphore.WaitAsync();
+            try
+            {
+                await Task.Delay(100);
+                _parts++;
+            }
+            finally
+            {
+                _deliverSemaphore.Release();
+            }
+        }
+
+        public async Task PickupAsync(int Id)
+        {
+            await _pickupSemaphore.WaitAsync();
+            try
+            {
+                if (_products > 0)
+                {
+                    await Task.Delay(100);
+                    _products--;
+                }
+            }
+            finally
+            {
+                _pickupSemaphore.Release();
+            }
+        }
+
+        public async Task ProcessAsync()
+        {
+            while (true)
+            {
+                await Task.Delay(60);
+                if (_parts > 0)
+                {
+                    _parts--;
+                    _products++;
+                }
+            }
+        }
+
+        public int GetPartsCount()
+        {
+            return _parts;
+        }
+
+        public int GetProductsCount()
+        {
+            return _products;
+        }
+    }
+}
---
+++ Factory/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Factory
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Factory factory = new Factory();
+            List<Task> tasks = new List<Task>();
+
+            for (int i = 1; i <= 100; i++)
+            {
+                int Id = i;
+                tasks.Add(Task.Run(async () =>
+                {
+                    while (true)
+                    {
+                        Console.WriteLine($"Machine {Id} is delivering parts...");
+                        await factory.DeliverAsync(Id);
+                        Console.WriteLine($"Machine {Id} delivered parts. Total parts: {factory.GetPartsCount()}");
+
+                        await Task.Delay(new Random().Next(60));
+                    }
+                }));
+            }
+
+            for (int i = 1; i <= 100; i++)
+            {
+                int Id = i;
+                tasks.Add(Task.Run(async () =>
+                {
+                    while (true)
+                    {
+                        Console.WriteLine($"Machine {Id} is picking up products...");
+                        await factory.PickupAsync(Id);
+                        Console.WriteLine($"Machine {Id} picked up products. Remaining products: {factory.GetProductsCount()}");
+
+                        await Task.Delay(new Random().Next(60));
+                    }
+                }));
+            }
+
+            tasks.Add(factory.ProcessAsync());
+
+            await Task.WhenAll(tasks);
+        }
+    }
+}
